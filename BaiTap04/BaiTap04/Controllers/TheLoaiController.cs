f39b8ba3a86abf419187a83639ef74389e1073ba
Nguyenhuy-gif14 <147743260+Nguyenhuy-gif14@users.noreply.github.com>
2024-09-04 14:34:55 +0700

    Hoan thanh bt4

---
+++ BaiTap04/BaiTap04/Controllers/TheLoaiController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap04.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return Ok($"Id: {id}");
+        }
+
+        public IActionResult Detail(int id, string ten)
+        {
+            return Content($"id: {id}; ten: {ten}");
+        }
+
+        public IActionResult Show(List<string> categories)
+        {
+            string content = "Danh sách thể loại:";
+            foreach (var category in categories)
+            {
+                content += " " + category;
+            }
+            return Content(content);
+        }
+    }
+}
