fffab0175ffff88e610cc95709a6559b7842d2df
Nguyenhuy-gif14 <147743260+Nguyenhuy-gif14@users.noreply.github.com>
2024-09-04 16:07:37 +0700

    hoan thanh vidu 05

---
+++ BT05/BT05/Controllers/TheLoaiController1.cs
@@ -0,0 +1,25 @@
+﻿using BT05.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BT05.Controllers
+{
+    public class TheLoaiController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Ngay = "Ngày 28";
+            ViewBag.Thang = "Thang 02";
+            ViewData["Nam"] = "Năm 2030";
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            var theLoai = new TheLoaiViewModel
+            {
+                Id = 1,
+                Name = "Trinh Tham"
+            };
+            return View(theLoai);
+        }
+    }
+}
