90c33fd079fd25477ec8dbb5d08ccafced9c31cc
Nguyenhuy-gif14 <147743260+Nguyenhuy-gif14@users.noreply.github.com>
2024-09-06 19:09:41 +0700

    Hoan Thanh Bai Tap Ve Nha

---
+++ BaiTapThucHanh02/BaiTapThucHanh02/Controllers/Tuan02Controller.cs
@@ -0,0 +1,20 @@
+﻿using BaiTapThucHanh02.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapThucHanh02.Controllers
+{
+    public class Tuan02Controller : Controller
+    {
+        public IActionResult Index()  
+        {
+            ViewBag.HoTen = "Nguyễn Trường Huy";
+            ViewBag.MSSV = "0123456789";
+            ViewBag.Nam = 2024;
+            
+            return View();
+
+        }
+
+
+    }
+}
---
+++ BaiTapVeNha03/BaiTapVeNha03/Controllers/MayTinhController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapVeNha03.Controllers
+{
+    public class MayTinhController : Controller
+    {
+        public IActionResult MayTinh(double a, double b, string pheptinh)
+        {
+            double ketQua = 0;
+            string error = null;
+
+            switch (pheptinh?.ToLower())
+            {
+                case "cong":
+                    ketQua = a + b;
+                    break;
+                case "tru":
+                    ketQua = a - b;
+                    break;
+                case "nhan":
+                    ketQua = a * b;
+                    break;
+                case "chia":
+                    if (b != 0)
+                        ketQua = a / b;
+                    else
+                        error = "Không thể chia cho 0";
+                    break;
+                default:
+                    error = "Phép tính không hợp lệ";
+                    break;
+            }
+
+            ViewBag.KetQua = ketQua;
+
+            return View();
+        }
+        
+    }
+}
