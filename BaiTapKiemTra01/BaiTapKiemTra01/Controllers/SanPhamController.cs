37f5597ba7b861d302c7e242052e72b065490cf4
Nguyenhuy-gif14 <147743260+Nguyenhuy-gif14@users.noreply.github.com>
2024-09-14 15:04:13 +0700

    Hoàn Thành Bài Tập Kiểm Tra 01

---
+++ BaiTapKiemTra01/BaiTapKiemTra01/Controllers/SanPhamController.cs
@@ -0,0 +1,25 @@
+﻿using BaiTapKiemTra01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapKiemTra01.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult BaiTap02()
+        {
+            // Giả sử có một sản phẩm với các thông tin sau:
+            var sanPham = new SanPhamViewModel
+            {
+                TenSanPham = "Máy tính xách tay Dell",
+                GiaBan = 15000000, 
+                AnhMoTa = "Laptopdell.jpg"
+            };
+            return View(sanPham);
+        }
+    }
+}
---
+++ BaiTapKiemTra01/BaiTapKiemTra01/Controllers/TaiKhoanController.cs
@@ -0,0 +1,25 @@
+﻿using BaiTapKiemTra01.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace BaiTapKiemTra01.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult DangKy(TaiKhoanViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Hiển Thị Dữ Liệu Nhập Vào
+                return Content($"Tên tài khoản: {model.TenTaiKhoan}, Họ tên: {model.HoTen}, Tuổi: {model.Tuoi}");
+            }
+
+            return View(model); //nếu có lỗi, quay lại form
+        }
+    }
+}
