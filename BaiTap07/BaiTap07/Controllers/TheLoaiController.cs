adb99fe131ca9b8dab2ac07b33895ffd20c66682
Nguyenhuy-gif14 <147743260+Nguyenhuy-gif14@users.noreply.github.com>
2024-09-18 14:56:22 +0700

    Update Bt07

---
+++ BaiTap07/BaiTap07/Controllers/TheLoaiController.cs
@@ -0,0 +1,36 @@
+﻿using BaiTap07.Data;
+using BaiTap07.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap07.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public TheLoaiController(ApplicationDbContext db)
+        {
+            _db= db;
+        }
+        public IActionResult Index()
+        {
+            var theLoai = _db.TheLoais.ToList();
+            ViewBag.theLoai = theLoai;
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Thread theloai)
+        {
+            //thêm vào nbangr Theloai
+            _ = _db.TheLoais.Add(theloai);
+            //lưu
+            _db.SaveChanges();
+            return View();
+        }
+    }
+}
